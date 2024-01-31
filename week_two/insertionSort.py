64e6cf9b7e8aacde90dbcbeababb5bd13ff16b53
santi-crown <samtigomusik@gmail.com>
2024-01-30 23:14:12 -0500

    add test file - comprehension of [:]

--- week_two/insertionSort.py
+++ week_two/insertionSort.py
@@ -5,10 +5,16 @@ def addElement(element, seq):
     while not (seq[element_index] > element):
         element_index = element_index + 1
     return seq[:element_index] + [element] + seq[element_index:] # is a sugar syntax to conveniently slice a list.
+
 def insertionSort(seq):
     result = seq[:START]
     for e in range(START,len(seq)):
+        # print( result)
+        # print(seq[e],result)
+        # print (result)
         result = addElement(seq[e], result)
+        # print(result)
+        # print( result)
     return result
 # def main():
 print(insertionSort([4,3,2,1,-5]))
---
+++ week_two/test.py
@@ -0,0 +1,38 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+
+element_index = 5
+# print(list[:element_index]) # output: [1, 2, 3, 4, 5]
+# print(list[element_index:]) # output: [6, 7, 8, 9, 10]
+
+
+# for e in range(1,len(list)):
+    # print(list[:e])
+    
+
+# output:[]
+# [1]
+# [1, 2]
+# [1, 2, 3]
+# [1, 2, 3, 4]
+# [1, 2, 3, 4, 5]
+# [1, 2, 3, 4, 5, 6]
+# [1, 2, 3, 4, 5, 6, 7]
+# [1, 2, 3, 4, 5, 6, 7, 8]
+# [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+# test adding a number before a number and after a {}
+
+l1 = [4]
+# print(l1[0:])
+e = 4
+# # print(l1[:1])
+
+result = l1[:1] + [e] + l1[1:]
+print(result)
+
+# print(result)
+
+seq = [4]
+
+# print(seq[1:])
