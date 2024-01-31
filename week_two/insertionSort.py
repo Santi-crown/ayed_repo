0092b84799936942fde5a68ac6fd2a817ee57b32
santi-crown <samtigomusik@gmail.com>
2024-01-30 21:45:44 -0500

    add insertion sort algorithm

---
+++ week_two/insertionSort.py
@@ -0,0 +1,15 @@
+START = 1
+
+def addElement(element, seq):
+    element_index = 0
+    while not (seq[element_index] > element):
+        element_index = element_index + 1
+    return seq[:element_index] + [element] + seq[element_index:] # is a sugar syntax to conveniently slice a list.
+def insertionSort(seq):
+    result = seq[:START]
+    for e in range(START,len(seq)):
+        result = addElement(seq[e], result)
+    return result
+# def main():
+print(insertionSort([4,3,2,1,-5]))
+# main()
