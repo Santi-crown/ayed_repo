514c5410f44e8035aa191c7ed51fd4d7d60924fc
santi-crown <samtigomusik@gmail.com>
2024-01-30 20:42:22 -0500

    Add menu

--- week_one/sparring.py
+++ week_one/sparring.py
@@ -1,7 +1,22 @@
+import os
+
+def delete_screen():
+    if os.name == "posix":
+        os.system("clear" )
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        os.system("cls")
+
+def menu():
+        print ("Selecciona una opción")
+        print ("\t1 - Character frequency")
+        print ("\t2 - Wonder Square")
+        print ("\t3 - Palindrome string")
+        print ("\t4 - Greatest Common Diviso")
+        print ("\t5 - salir")
 
 def get_user_input(type_input):
     if type_input == 1:
-        user_input = input("Please type your input: ")
+        user_input = input("Please type your string: ")
         return user_input
     elif type_input == 2:
         number_1, number_2 = int(input("Type the first number: ")), int(input("Type the first number: "))
@@ -12,7 +27,7 @@ def get_user_input(type_input):
             user_input = int(input("Type the size, please: "))
         return user_input
 
-# Exercise one
+# 1. 
 def character_frequency(word):
     times = 0
     most_frequently = ""
@@ -32,7 +47,7 @@ def count(ci,word):
             frequency = frequency + 1
     return frequency
 
-# Exercise two
+# 2. 
 def generate_wonder_square(n):
     # This is because we need the matrix to be of odds size so it can form the WonderSquare
     size = 2 * n - 1
@@ -51,8 +66,7 @@ def print_wonder_square(wonder_square):
         print(" ".join(map(str, row)))
 
 
-
-# Exercise three
+# 3. 
 def palindrome(word):    
     word_validation = []
     for element in range(len(word)-1,-1,-1):
@@ -62,9 +76,8 @@ def palindrome(word):
     else:
         print("False")       
 
-# Exercise four
+# 4. 
 # Euclidean Algorithm 
-
 # This algorithm repeatedly divides the larger number by the smaller number and assigns the remainder as the new divisor until the remainder becomes zero. Then, the last non-zero remainder is the gcd.
 def gcd_two_numbers(n_1,n_2):
     while n_2 != 0:    
@@ -73,33 +86,41 @@ def gcd_two_numbers(n_1,n_2):
 
 
 def main():    
-    #data
-    # word = get_user_input(1)
-    # word_as_list = list(word)
+    while True:
+        menu()
+        opcionMenu = input("inserta un numero valor >> ")
 
-    # n_1, n_2 = get_user_input(2)
+        if opcionMenu=="1":
+            # 1. 
+            word = get_user_input(1)
+            word_as_list = list(word)
+            character_frequency(word)
+            input("Ha seleccionado la opción 1...\npulsa una tecla para continuar ")
+        elif opcionMenu=="2":            
+            # 2. 
+            # here we generate the matrix
+            n = get_user_input(3)
+            wonder_square = generate_wonder_square(n)
+            # Here, we give it a print format
+            print_wonder_square(wonder_square)
+            input("Ha seleccionado la opción 2...\npulsa una tecla para continuar")
 
-    n = get_user_input(3)
+        elif opcionMenu=="3":
+            # 3.  
+            word = get_user_input(1)
+            string_as_list = list(word)        
+            palindrome(word_as_list)                        
+            input("Ha seleccionado la opción 3...\npulsa una tecla para continuar")
 
-# - - - - -- - 
+        elif opcionMenu=="4":        
+            # 4.
+            n_1, n_2 = get_user_input(2)
+            gcd = gcd_two_numbers(n_1,n_2)
+            print(f"The GCD of {n_1} and {n_2} is {gcd}")
+            input("Ha seleccionado la opción 4...\npulsa una tecla para continuar")
 
-    # 1. Exercise one
-    # character_frequency(word)
-
-    # 2. Exercise two
-    # here we generate the matrix
-    wonder_square = generate_wonder_square(n)
-    # Here, we give it a print format
-    print_wonder_square(wonder_square)
-
-
-    # 3.  Exercise three
-    # palindrome(word_as_list)
-
-    # 4.  Exercise four
-    # gcd = gcd_two_numbers(n_1,n_2)
-    # print(f"The GCD of {n_1} and {n_2} is {gcd}")
-
+        elif opcionMenu=="5":
+            input("Ha seleccionado la opción 5...\npulsa una tecla para continuar")
+            print("Chao Chao")
+            break
 main()
-
-
