97ce8fe8460af9962555c0e0e9ee666dc16ff221
santi-crown <samtigomusik@gmail.com>
2024-01-30 23:26:39 -0500

    add MyInsertionShort file - i make the code

--- week_two/insertionSort.py
+++ week_two/insertionSort.py
@@ -5,6 +5,7 @@ def addElement(element, seq):
     while not (seq[element_index] > element):
         element_index = element_index + 1
     return seq[:element_index] + [element] + seq[element_index:] # is a sugar syntax to conveniently slice a list.
+    # The key point here is to insert into two elements, initially, it's between [] and [4], then, when e = 1, it's at [3] because counting starts from 1
 
 def insertionSort(seq):
     result = seq[:START]
