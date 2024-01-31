bc330a44a1ab8d7805ab8808c3c1483d919d7b74
santi-crown <samtigomusik@gmail.com>
2024-01-30 20:16:53 -0500

    Add exercise four - without understanding

--- week_one/sparring.py
+++ week_one/sparring.py
@@ -3,9 +3,14 @@ def get_user_input(type_input):
     if type_input == 1:
         user_input = input("Please type your input: ")
         return user_input
-    else:
+    elif type_input == 2:
         number_1, number_2 = int(input("Type the first number: ")), int(input("Type the first number: "))
         return number_1, number_2
+    else:
+        user_input = -1
+        while user_input < 0:
+            user_input = int(input("Type the size, please: "))
+        return user_input
 
 # Exercise one
 def character_frequency(word):
@@ -28,9 +33,26 @@ def count(ci,word):
     return frequency
 
 # Exercise two
+def generate_wonder_square(n):
+    # This is because we need the matrix to be of odds size so it can form the WonderSquare
+    size = 2 * n - 1
+
+    # Here we're initializing the 0 matrix with the odd size
+    matriz = [[0 for i in range(size)] for j in range(size)]
+
+    # Here we are going to fill the matrix. 
+    for i in range(n):  
+        for j in range(i, size - i):
+            matriz[i][j] = matriz[j][i] = matriz[size - i - 1][j] = matriz[j][size - i -1] = n - i
+    return matriz
+
+def print_wonder_square(wonder_square):
+    for row in wonder_square:
+        print(" ".join(map(str, row)))
+
+
 
 # Exercise three
-
 def palindrome(word):    
     word_validation = []
     for element in range(len(word)-1,-1,-1):
@@ -54,17 +76,30 @@ def main():
     #data
     # word = get_user_input(1)
     # word_as_list = list(word)
-    n_1, n_2 = get_user_input(2)
 
-    #Exercise one
+    # n_1, n_2 = get_user_input(2)
+
+    n = get_user_input(3)
+
+# - - - - -- - 
+
+    # 1. Exercise one
     # character_frequency(word)
 
-    # Exercise three
+    # 2. Exercise two
+    # here we generate the matrix
+    wonder_square = generate_wonder_square(n)
+    # Here, we give it a print format
+    print_wonder_square(wonder_square)
+
+
+    # 3.  Exercise three
     # palindrome(word_as_list)
 
-    # Exercise four
-    gcd = gcd_two_numbers(n_1,n_2)
-    print(f"The GCD of {n_1} and {n_2} is {gcd}")
+    # 4.  Exercise four
+    # gcd = gcd_two_numbers(n_1,n_2)
+    # print(f"The GCD of {n_1} and {n_2} is {gcd}")
+
 main()
 
 
