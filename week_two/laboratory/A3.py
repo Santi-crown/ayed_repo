2bf4bd095e3822fa766de72366a5248c82fb04bc
santi-crown <samtigomusik@gmail.com>
2024-02-09 19:25:00 -0500

    add complexity to A3

--- week_two/laboratory/A3.py
+++ week_two/laboratory/A3.py
@@ -1,25 +1,22 @@
 from sys import stdin
-
-def write(line):
-    words = []
-    index = 0
-    for i in line:
-        if len(i) > index:
-            words.append(i[index])
-            index += 1
-    words = map(str, words)
-    print("".join(words))
-    return index
-
-def main():
-    rango = int(stdin.readline().strip())
-    empty = stdin.readline()
-    for i in range(rango):
-        print("Case #" + str(i + 1) + ":")
-        line = stdin.readline().strip().split(" ")
-        # print(line)
-        while len(line[0]) > 0:
-            write(line)
-            line = stdin.readline().strip().split(" ")
-            # print(line)
-main()
+def write(line):                                                # costo   pasos
+    words = []                                                  #   1       1
+    index = 0                                                   #   1       1
+    for i in line: #ciclo de la invariante #1                   #   1       n
+        if len(i) > index:                                      #   1       n - 1
+            words.append(i[index])                              #   1       n - 1
+            index += 1                                          #   1       n - 1
+    words = map(str, words)                                     #   1       1
+    print("".join(words))                                       #   1       1
+    return index                                                #   1       1
+                                                                #Total O = (n), omega = 1
+def main():                                                     # costo   pasos
+    rango = int(stdin.readline().strip())                       #   1       1
+    empty = stdin.readline()                                    #   1       1
+    for i in range(rango):#ciclo de la invariante #2            #   1       j
+        print("Case #" + str(i + 1) + ":")                      #   1       j - 1
+        line = stdin.readline().strip().split(" ")              #   1       j - 1
+        while len(line[0]) > 0:                                 #   1       j - 1
+            write(line)                                         # write(n)  j - 1
+            line = stdin.readline().strip().split(" ")          # 1         j - 1
+main()                                                          #Total O = (n), omega = 1
