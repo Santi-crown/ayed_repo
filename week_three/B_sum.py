b2dbe33725d753dc2be04e462aa966e70d55d9bb
santi-crown <samtigomusik@gmail.com>
2024-02-14 20:14:00 -0500

    Add tell_spots.py

--- week_three/B_sum.py
+++ week_three/B_sum.py
@@ -1,6 +1,22 @@
+from sys import stdin
+
 def summation(i,j,numbers = []):
     if i > j or i >= len(numbers):
         return 0
     return numbers[i] + summation(i+1,j,numbers) # invariante
 
-print(summation(8,9, [1,2,3,4,5,6,7,8,9,10]))
+#print(summation(8,9, [1,2,3,4,5,6,7,8,9,10]))
+
+line1 = stdin.readline().strip()
+line2 = stdin.readline().strip()
+line3 = stdin.readline().strip().split()
+
+print(line1,line2,line3)
+
+while len(line1) > 0 and len(line2) > 0 and len(line3) > 0:
+    numbers = [int(num) for num in line3]
+    i, j, sequence = int(line1), int(line2), line3
+    print(summation(i, j, sequence))
+    line1 = stdin.readline().strip()
+    line2 = stdin.readline().strip()
+    line3 = stdin.readline().strip().split()
