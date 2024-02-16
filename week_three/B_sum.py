30ad35964c9fcb1fb4394504a43773893024968b
santi-crown <samtigomusik@gmail.com>
2024-02-16 18:28:30 -0500

    Add stdin to B_sum.py

--- week_three/B_sum.py
+++ week_three/B_sum.py
@@ -7,21 +7,12 @@ def summation(i,j,numbers = []):
 
 #print(summation(8,9, [1,2,3,4,5,6,7,8,9,10]))
 
-line1 = int(stdin.readline().strip())
-line2 = int(stdin.readline().strip())
+i = int(stdin.readline().strip())
+j = int(stdin.readline().strip())
 line3 = stdin.readline().strip().split()
-
-print(line3)
 # We map each string as int
 line3_int = list(map(int,line3))
-print(line3_int)
+print(summation(i,j,line3_int))
 
-print(summation(line1,line2,line3_int))
 
-# while len(line1) > 0 and len(line2) > 0 and len(line3) > 0:
-#     numbers = [int(num) for num in line3]
-#     i, j, sequence = int(line1), int(line2), line3
-#     print(summation(i, j, sequence))
-#     line1 = stdin.readline().strip()
-#     line2 = stdin.readline().strip()
-#     line3 = stdin.readline().strip().split()
+
