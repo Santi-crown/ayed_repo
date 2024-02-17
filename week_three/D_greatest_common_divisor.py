064ef5caf73e9546cf6f6e3c23793ea6d9107408
santi-crown <samtigomusik@gmail.com>
2024-02-17 09:15:52 -0500

    Add stdin to D_greatest_common_divisor.py just for one case

--- week_three/D_greatest_common_divisor.py
+++ week_three/D_greatest_common_divisor.py
@@ -1,6 +1,15 @@
+from sys import stdin
+
 def gcd(m,n):
     if n == 0:
         return m
     return gcd(n,m % n) # invariante
 
-print(gcd(100,95))
+# print(gcd(100,95))
+
+inputs = stdin.readline().strip().split()
+int_inputs = list(map(int,inputs))
+
+n,m = int_inputs[0], int_inputs[1]
+
+print(gcd(n,m))
