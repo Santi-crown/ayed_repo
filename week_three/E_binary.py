0abbdb9d7baaad270acae95c363297002dcfdb96
santi-crown <samtigomusik@gmail.com>
2024-02-17 10:28:52 -0500

    Add stdin for each case to E_binary.py

--- week_three/E_binary.py
+++ week_three/E_binary.py
@@ -1,6 +1,15 @@
+from sys import stdin
+
 def binary(n):
     if n // 2 == 0:
         return [n % 2]
     return [n % 2] + binary(n // 2) # invariante
 
-print(binary(5))
+n = stdin.readline().strip()
+
+while n != '':
+    binary_list = binary(int(n))
+    binary_list.reverse()
+    # print each list of binary as a string concatenates
+    print(''.join(map(str,binary_list)))
+    n = stdin.readline().strip()
