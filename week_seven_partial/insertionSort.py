f28589972cbaf6329b1316f033116f2fea2760ea
santi-crown <samtigomusik@gmail.com>
2024-03-05 18:18:26 -0500

    Add correction insertionSort to my insertionSort codes

--- week_seven_partial/insertionSort.py
+++ week_seven_partial/insertionSort.py
@@ -1,83 +1,38 @@
-# Example
 # START = 1
-# def addElement(element, seq):
-#     element_index = 0
-#     while not (seq[element_index] > element):
-#         element_index = element_index + 1
-#     return seq[:element_index] + [element] + seq[element_index:]
-# def insertionSort(seq):
-#     result = seq[:START]
-#     for e in range(START,len(seq)):
-#         result = addElement(seq[e], result)
-#     return result
-# print(insertionSort([4,3,2,1,-4]))
-
-
-# Try 1
-# START = 1
-
-# def addElement(element, seq):
-#     element_index = 0
-#     while seq[element_index] < element:
-#         element_index += 1
-#     return seq[:element_index] + [element] + seq[element_index:]
+# def insertionSortElement(new_el, sequence):
+#     index = 0
+#     while index < len(sequence) and new_el > sequence[index]:
+#         index+=1
+#     return sequence[:index] + [new_el] + sequence[index:]
 #
-# def insertionSort(seq):
-#     result = seq[:START]
-#     for e in range(START, len(seq)):
-#         result = addElement(seq[e], result)
-#     return result
-#
-# print(insertionSort([4,3,2,1,-5]))
+# def insertionSort(A):
+#     for index in range(START,len(A)):
+#         elem = A[index]
+#         A = insertionSortElement(elem, A[:index]) + A[index+1:]
+#     return A
 
-# Try 2
-# START = 1
-
-# def addElement(seq,element):
-#     element_index = 0
-#     while seq[element_index] < element:
-#         element_index += 1
-#     return seq[:element_index] + [element] + seq[element_index:]
+# try 1
+# def insertionSortElement(new_el,sequence):
+#     index = 0
+#     while index < len(sequence) and new_el > sequence[index]:
+#         index+=1
+#     return sequence[:index] + [new_el] + sequence[index:]
 #
-# def insertionSort(seq):
-#     result = seq[:START]
-#     for e in range(START, len(seq)):
-#         result = addElement(result, seq[e])
-#     return result
+# def insertionSort(sequence):
+#     for index in range(1,len(sequence)):
+#         sequence = insertionSortElement(sequence[index],sequence[:index]) + sequence[index+1:]
+#     return sequence
 #
-# print(insertionSort([4,3,2,1,-5]))
 
-# Try 3
-#START = 1
+# try 2
+def insertionSortElement(new_el,sequence):
+    index = 0
+    while index < len(sequence) and new_el > sequence[index]:
+        index += 1
+    return sequence[:index] + [new_el] + sequence[index:]
 
-# def addElement(element, seq):
-#     element_index = 0
-#     while seq[element_index] < element:
-#         element_index += 1
-#     return seq[:element_index] + [element] + seq[element_index:]
-#
-#
-# def insertionSort(seq):
-#     result = seq[:START]
-#     for e in range(START, len(seq)):
-#         result = addElement(seq[e], result)
-#     return result
-#
-#
-# print(insertionSort([4,3,2,1,-5]))
-
-# Try 4
-START = 1
-def addElement(element, seq):
-    element_index = 0
-    while seq[element_index] < element:
-        element_index += 1
-    return seq[:element_index] + [element] + seq[element_index:]
-
-
-def insertionSort(seq):
-    result = seq[:START]
-    for e in range(START,len(seq)):
-        result = addElement(seq[e],result)
-    return result
-print(insertionSort([4,3,2,1,-5]))
+def insertionSort(sequence):
+    for index in range(1,len(sequence)):
+        sequence = insertionSortElement(sequence[index],sequence[:index]) + sequence[index+1:]
+    return sequence
+print(insertionSort([4,1,9,3,101,66,8,100]))
