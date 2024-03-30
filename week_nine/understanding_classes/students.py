28affd9bc193cac715a8375b8fde059335d48b31
santi-crown <samtigomusik@gmail.com>
2024-03-30 17:12:36 -0500

    Teacher's example - students

---
+++ week_nine/understanding_classes/students.py
@@ -0,0 +1,58 @@
+from uuid import uuid1
+from random import randint
+class Estudiante:
+
+    def __init__(self, nombre, id):
+        #Seccion de declaracion de atributos
+        self.nombre = ""
+        self.id = None
+        self.notas = {}
+        self.gpa = 0.0
+        #Seccion de asignación de atributos
+        self.setNombre(nombre)
+        self.setId(id)
+
+    def getNombre(self):
+        return self.nombre
+
+    def setNombre(self, nombre):
+        if not isinstance(nombre, str):
+            raise Exception("El nombre no corresponde al tipo de dato esperado")
+        self.nombre = nombre
+
+    def getId(self):
+        return self.id
+
+    def setId(self, id):
+        if not (isinstance(id, int) or int is None):
+            raise Exception("El id no corresponde al tipo de dato esperado")
+        self.id = id
+
+    def getNotas(self):
+        return self.notas
+
+    def añadirNota(self, nota):
+        self.notas[nota[0]] = nota[1]
+        self.calcularGPA()
+
+    def calcularGPA(self):
+        totalNotas = len(self.notas.keys())
+        if totalNotas > 0:
+            self.gpa = sum(self.notas.values())/totalNotas
+
+    def __str__(self):
+        return "Estudiante({}, {}, {}, {})".format(self.id, self.nombre, self.notas, self.gpa)
+
+
+def main():
+    estudiantes = [ Estudiante(str(uuid1()), randint(0,int(1e6))) for i in range(10)]
+    for e in estudiantes:
+        notas = [(str(uuid1()), randint(15,50)/10) for i in range(3)]
+        for nota in notas:
+            e.añadirNota(nota)
+
+    for e in estudiantes:
+        print(e)
+
+
+main()
--- week_nine/understanding_classes/wizard.py
+++ week_nine/understanding_classes/wizard.py
@@ -1,3 +1,29 @@
+# class Wizard:
+#     def __init__(self, name, nick_name, house, blood):
+#         self.name = name
+#         self.nick_name = nick_name
+#         self.house = house
+#         self.blood = blood
+#         self.chosen_one = False
+#     def chosen(self):
+#         chosen_one = True
+#         print(self.name," is the chosen one")
+#
+# Hermione = Wizard('Hermione Grenger', 'None', 'Gryffinder', 'Mudblood')
+# Harry = Wizard('Harry Potter', 'The Boy Who Lived', 'Gryffindor', 'half-blood')
+# Ron = Wizard('Ron Weasley', 'Ron', 'Gryffindor','Pure-blood')
+# Voldemort = Wizard('Tom Marvolo Riddle', 'Voldemort,  He-Who-Must-Not-Be-Named', 'Slytherin', 'Half-blood')
+#
+# print(type(Hermione))
+# print(type(Harry))
+# print(type(Ron))
+# print(type(Voldemort))
+#
+# print(Hermione.name,Hermione.nick_name,Hermione.house,Hermione.blood,Hermione.chosen_one)
+# Harry.chosen()
+# print(Harry.name,Harry.nick_name,Harry.house,Harry.blood,Harry.chosen_one)
+
+# <======================== Implenting getters and setters - To control the data type ========================>
 class Wizard:
     def __init__(self, name, nick_name, house, blood):
         self.name = name
@@ -9,16 +35,15 @@ class Wizard:
         chosen_one = True
         print(self.name," is the chosen one")
 
-Hermione = Wizard('Hermione Grenger', 'None', 'Gryffinder', 'Mudblood')
-Harry = Wizard('Harry Potter', 'The Boy Who Lived', 'Gryffindor', 'half-blood')
-Ron = Wizard('Ron Weasley', 'Ron', 'Gryffindor','Pure-blood')
-Voldemort = Wizard('Tom Marvolo Riddle', 'Voldemort,  He-Who-Must-Not-Be-Named', 'Slytherin', 'Half-blood')
+    def getName(self):
+        return self.name
+    def setName(self, name):
+        # With isinstance, we are checking if something is an instance of a certain class. For example, here we are checking if the name is a string.
+        if not isinstance(name, str):
+            raise Exception("The name does not correspond to the expected data type")
+        self.name = name
 
-print(type(Hermione))
-print(type(Harry))
-print(type(Ron))
-print(type(Voldemort))
+# Hermione = Wizard(4,'None', 'Gryffinder', 'Mudblood')
 
-print(Hermione.name,Hermione.nick_name,Hermione.house,Hermione.blood,Hermione.chosen_one)
-Harry.chosen()
-print(Harry.name,Harry.nick_name,Harry.house,Harry.blood,Harry.chosen_one)
+# print(Hermione.name,Hermione.nick_name,Hermione.house,Hermione.blood,Hermione.chosen_one)
+
