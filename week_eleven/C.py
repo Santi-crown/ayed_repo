ba18a538a3b7ade1cd04e3e586736a3666cb9f50
santi-crown <samtigomusik@gmail.com>
2024-04-14 17:50:53 -0500

    Add stdin to C.py - perentheses balanced, my implementatio

--- week_eleven/C.py
+++ week_eleven/C.py
@@ -1,32 +1,27 @@
 # Data structure : stack
-openSymbols = []
-closedSymbols = []
-# [] --> "A"
-# () --> "P"
-# [( --> True
-# )] --> False
-# Si se intenta sacar algo de una pila que esta vacía, genera underfloor, está mal.
-# Para implementar A y P, se puede hacer con un diccionario.
-# ([])
-balanced = True
-u_input = "([])"
-for symbol in u_input:
-    if symbol in "[(":
-        openSymbols.append(symbol)
-    elif symbol == ')' and openSymbols and openSymbols[-1] == '(':
-        openSymbols.pop()
-    elif symbol == ']' and openSymbols and openSymbols[-1] == '[':
-        openSymbols.pop()
-    else:
-        closedSymbols.append(symbol)
-if openSymbols or closedSymbols:
-    balanced = False
-# print(openSymbols)
-# print(closedSymbols)
-print(balanced)
-# Casos de aceptación
-# openSymbols es vacio
+from sys import stdin
 
-#Casos de no aceptación
-# openSymbols no vacio
-# Se intentan sacar parentesis de openSymbols vacio - underfloor
+
+def is_balanced(u_input):
+    openSymbols = []
+    closedSymbols = []
+    balanced = "Yes"
+    for symbol in u_input:
+        if symbol in "[(":
+            openSymbols.append(symbol)
+        elif symbol == ')' and openSymbols and openSymbols[-1] == '(':
+            openSymbols.pop()
+        elif symbol == ']' and openSymbols and openSymbols[-1] == '[':
+            openSymbols.pop()
+        else:
+            closedSymbols.append(symbol)
+    if openSymbols or closedSymbols:
+        balanced = "No"
+    return balanced
+# print(balanced)
+def main():
+    n = int(stdin.readline())
+    for i in range(n):
+        line = stdin.readline().strip()
+        print(is_balanced(line))
+main()
