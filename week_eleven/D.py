c66f5088aaeb89dde21bbd0ea3c56b76e76e0714
santi-crown <samtigomusik@gmail.com>
2024-04-15 18:00:31 -0500

    Add solution just for the problem input example

--- week_eleven/D.py
+++ week_eleven/D.py
@@ -3,17 +3,45 @@
 # 2. si se quiere hacer dequeue y la lista 2 esta vacía, se pasara todos los elementos de la stack1 a la dos, donde quedaran invertidos+
 #   Si no esta vacía, solo se hace pop al stack2
 # Ya
-stack1 = []
-stack2 = []
-for i in range(6):
-    stack1.append(i+1)
-print(stack1)
-while stack1:
-    stack2.append(stack1.pop())
-print(stack2)
-print(stack2.pop())
-# print(stack1)
-# stack1.append(11)
-# print(stack1)
-# print(stack1.pop())
-# print(stack1)
+class QueueWithTwoStacks:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+
+    def enqueue(self, values):
+        self.stack1.append(values)
+    def dequeue(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+            return self.stack2.pop()
+        else:
+            return self.stack2.pop()
+    def peek(self):
+        if self.stack1:
+            return self.stack1[0]
+        elif self.stack2:
+            return self.stack2[-1]
+        else:
+            return None #Caso que ambas están vacias.
+    def __str__(self):
+        return str(self.stack1)
+
+myQueue = QueueWithTwoStacks()
+
+myQueue.enqueue(42)
+myQueue.dequeue()
+myQueue.enqueue(14)
+print(myQueue.peek())
+myQueue.enqueue(28)
+print(myQueue.peek())
+myQueue.enqueue(60)
+myQueue.enqueue(78)
+myQueue.dequeue()
+myQueue.dequeue()
+#print(myQueue)
+
+
+
+
